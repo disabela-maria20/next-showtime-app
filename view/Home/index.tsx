a2eb94f0282834d3a6a7dc0fc6666eaa2f359383
Isabela <isabela10014@hotmail.com>
2026-02-19 22:09:18 -0300

    feat: update cadastro home

--- view/Home/index.tsx
+++ view/Home/index.tsx
@@ -54,9 +54,6 @@ const Home = () => {
     [moviesByDate]
   );
 
-  // 🎯 data ativa
-
-  // ✅ selecionar HOJE automaticamente
   useEffect(() => {
     const today = new Date().toISOString().split('T')[0];
 
@@ -225,7 +222,7 @@ const Home = () => {
         </section>
         {!isMobile && <Divider />}
         <section className="py-14 md:py-32 overflow-hidden">
-          <div className="container max-w-490 m-auto px-12">
+          <div className="container max-w-490 m-auto p-3.5 md:px-12">
             {/* 🎬 DATAS */}
             <h2
               className="text-2xl md:text-4xl 2xl:text-5xl mb-6 md:mb-12 text-amber-400"
@@ -259,10 +256,20 @@ const Home = () => {
 
             <div className="grid grid-cols-2 grid-rows-3 gap-2 mb-5 xl:grid-cols-6 lg:grid-rows-1">
               <div className="">
-                <input type="text" name="pesquisar" id="pesquisar" className='w-full p-2 border border-amber-400 placeholder:text-amber-400 bg-black text-amber-400 text-sm rounded' placeholder="Pesquisar filmes..." />
+                <input
+                  type="text"
+                  name="pesquisar"
+                  id="pesquisar"
+                  className="w-full p-2 border border-amber-400 placeholder:text-amber-400 bg-black text-amber-400 text-sm rounded"
+                  placeholder="Pesquisar filmes..."
+                />
               </div>
               <div className="">
-                <select name="genero" id="genero" className='w-full p-2 border border-amber-400 bg-black text-amber-400 text-sm rounded'>
+                <select
+                  name="genero"
+                  id="genero"
+                  className="w-full p-2 border border-amber-400 bg-black text-amber-400 text-sm rounded"
+                >
                   <option value="">Todos os gêneros</option>
                   <option value="acao">Ação</option>
                   <option value="comedia">Comédia</option>
@@ -270,19 +277,31 @@ const Home = () => {
                 </select>
               </div>
               <div className="">
-                <select name="cinema" id="cinema" className='w-full p-2 border border-amber-400 bg-black text-amber-400 text-sm rounded'>
+                <select
+                  name="cinema"
+                  id="cinema"
+                  className="w-full p-2 border border-amber-400 bg-black text-amber-400 text-sm rounded"
+                >
                   <option value="">Todos os cinemas</option>
                   <option value="cinema1">Cinema 1</option>
                   <option value="cinema2">Cinema 2</option>
                   <option value="cinema3">Cinema 3</option>
                 </select>
               </div>
-              <div className='grid grid-cols-2 gap-2 items-center'>
-                <button className='w-full p-2 border border-amber-400 bg-black text-amber-400 text-sm rounded'>Dublado</button>
-                <button className='w-full p-2 border border-amber-400 bg-black text-amber-400 text-sm rounded'>Legendado</button>
+              <div className="grid grid-cols-2 gap-2 items-center">
+                <button className="w-full p-2 border border-amber-400 bg-black text-amber-400 text-sm rounded">
+                  Dublado
+                </button>
+                <button className="w-full p-2 border border-amber-400 bg-black text-amber-400 text-sm rounded">
+                  Legendado
+                </button>
               </div>
               <div className="">
-                <select name="cinema" id="cinema" className='w-full p-2 border border-amber-400 bg-black text-amber-400 text-sm rounded'>
+                <select
+                  name="cinema"
+                  id="cinema"
+                  className="w-full p-2 border border-amber-400 bg-black text-amber-400 text-sm rounded"
+                >
                   <option value="">tecnologia</option>
                   <option value="cinema1">tecnologia 1</option>
                   <option value="cinema2">tecnologia 2</option>
@@ -290,12 +309,14 @@ const Home = () => {
                 </select>
               </div>
               <div>
-                <button className='w-full p-2 border border-amber-400 bg-amber-400 text-black text-sm rounded'>buscar filmes</button>
+                <button className="w-full p-2 border border-amber-400 bg-amber-400 text-black text-sm rounded">
+                  buscar filmes
+                </button>
               </div>
             </div>
             {/* 🎥 FILMES */}
 
-             <div className="mb-5 md:hidden ">
+            <div className="mb-5 md:hidden ">
               <Slide
                 key={activeDate}
                 options={{
@@ -308,12 +329,11 @@ const Home = () => {
                 }}
               >
                 <Slide.Track style={{ overflow: 'visible' }}>
-                   {activeDate &&
+                  {activeDate &&
                     moviesByDate[activeDate]?.map((movie: any) => (
-                      <Slide.Item key={movie.id} >
-                        <CardMovie  {...movie} />
+                      <Slide.Item key={movie.id}>
+                        <CardMovie {...movie} />
                       </Slide.Item>
-                    
                     ))}
                 </Slide.Track>
               </Slide>
@@ -323,6 +343,115 @@ const Home = () => {
                 moviesByDate[activeDate]?.map((movie: any) => (
                   <CardMovie key={movie.id} {...movie} />
                 ))}
+            </div>
+          </div>
+        </section>
+        <section className="">
+          <div className="">
+            <div className="md:grid grid-cols-2 gap-8 items-center">
+              {/* Coluna da Imagem */}
+              <div
+                className="relative mb-8 md:mb-0
+                  before:content-['']
+                  before:absolute
+                  before:top-0
+                  before:right-0
+                  before:h-full
+                  before:w-full 
+                  before:bg-linear-to-t
+                  before:from-black/95
+                  before:to-transparent
+                  before:pointer-events-none
+                  before:z-10 md:before:content-none"
+              >
+                <img
+                  src={text.secao5.img.src}
+                  alt={text.secao5.img.alt}
+                  className="w-full md:h-screen object-cover relative"
+                />
+
+                {/* Texto sobreposto apenas no mobile */}
+                <div className="absolute bottom-0 left-0 right-0 p-8 z-20 md:hidden">
+                  <h2 className="text-2xl font-bold mb-2.5 text-white">
+                    {text.secao5.title}
+                  </h2>
+                  <p className="text-white/90">{text.secao5.description}</p>
+                </div>
+              </div>
+
+              {/* Coluna do Formulário */}
+              <div className="px-4 md:px-8">
+                {/* Texto apenas no desktop */}
+                <div className="hidden md:block mb-8">
+                  <h2 className="text-2xl md:text-4xl 2xl:text-5xl font-bold mb-4 text-white">
+                    {text.secao5.title}
+                  </h2>
+                  <p className="text-white/80">{text.secao5.description}</p>
+                </div>
+
+                <form action="" className="space-y-4">
+                  {/* Campo Nome */}
+                  <div>
+                    <label htmlFor="nome" className="block mb-1">
+                      <span className="text-blue-600 font-medium">Nome:</span>
+                    </label>
+                    <input
+                      id="nome"
+                      type="text"
+                      className="w-full px-4 py-2.5 bg-black border border-blue-600 rounded-lg text-white focus:border-b-blue-600 focus:outline-none transition"
+                      placeholder="Seu nome completo"
+                    />
+                  </div>
+
+                  {/* Grid para Telefone e Email */}
+                  <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+                    <div>
+                      <label htmlFor="tel" className="block mb-1">
+                        <span className="text-blue-600 font-medium">
+                          Telefone:
+                        </span>
+                      </label>
+                      <input
+                        id="tel"
+                        type="tel"
+                        className="w-full px-4 py-2.5 bg-black border border-blue-600 rounded-lg text-white focus:border-blue-600 focus:outline-none transition"
+                        placeholder="(11) 99999-9999"
+                      />
+                    </div>
+                    <div>
+                      <label htmlFor="email" className="block mb-1">
+                        <span className="text-blue-600 font-medium">
+                          Email:
+                        </span>
+                      </label>
+                      <input
+                        id="email"
+                        type="email"
+                        className="w-full px-4 py-2.5 bg-black border border-blue-600 rounded-lg text-white focus:border-amber-400 focus:outline-none transition"
+                        placeholder="seu@email.com"
+                      />
+                    </div>
+                  </div>
+
+                  {/* Checkbox de termos */}
+                  <div className="flex items-center py-4">
+                    <input
+                      type="checkbox"
+                      name="termos"
+                      id="termos"
+                      className="w-4 h-4 accent-amber-400 mr-3"
+                    />
+                    <label htmlFor="termos" className="text-sm text-white/80">
+                      {text.secao5.terms}
+                    </label>
+                  </div>
+
+                  {/* Botão de submit */}
+                 <CtaButton className="w-full">
+                    cadastrar
+                  </CtaButton>
+                </form>
+              </div>
             </div>
           </div>
         </section>
