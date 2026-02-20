1ac96c764a00f20c456ce6916f7725249af73cad
Isabela <isabela10014@hotmail.com>
2026-02-19 22:10:20 -0300

    feat: update css

--- component/Divider/index.tsx
+++ component/Divider/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const Divider = () => {
-  return <img src="./img/faixa-gradiente.png" alt="Divider" title="Divider" />;
+  return <img src="./img/faixa-gradiente.png" className='w-full' alt="Divider" title="Divider" />;
 };
 
 export default Divider;
