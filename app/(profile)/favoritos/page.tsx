5f5035e450233aa2438d71f49eb4aa7561272690
Isabela Maria Ribeiro Da Silva <imsilva9@topazevolution.com>
2026-04-23 16:56:43 -0300

    feat: ajuste de paginas

--- app/(profile)/favoritos/page.tsx
+++ app/(profile)/favoritos/page.tsx
@@ -1,8 +1,24 @@
+
+'use client';
+
 import { listHomeMovies } from '@/services/api';
 import { Movie } from '@/services/models';
-import { Favorites } from '@/screens';
+
+import { CardMovie } from '@/component';
+import { useMemo } from 'react';
 
 export default async function pageFavorites() {
   const listMovies = await listHomeMovies() as { releases: Movie[]; streaming: Movie[] };
-  return <Favorites movie={listMovies} />
+    const movies = useMemo(() => listMovies.releases.concat(listMovies.streaming), [listMovies]);
+
+  return  <section>
+      <h1 className="text-white text-4xl text-center md:text-left font-bold mb-8">
+        Seus Favoritos
+      </h1>
+      <div className="grid grid-cols-2 md:grid-cols-4 gap-4 md:gap-7">
+        {movies.map((item, i) => (
+          <CardMovie index={i} {...item} favorites={true} />
+        ))}
+      </div>
+    </section>
 }
--- features/index.ts
+++ features/index.ts
@@ -1,7 +1,6 @@
-export { default as Home } from './Home';
+
 export { default as Film} from './Film';
-export { default as Favorites} from './Favorites';
-export { default as About} from './About';
+
 export { default as Shortly} from './Shortly';
 export { default as NewsPost} from './NewsPost';
 export { default as NewsPage} from './NewsPage';
